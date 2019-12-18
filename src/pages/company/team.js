65a68edc5756a31911597ba36e96a82ccc7320b3
Gerard Hynes <gkhynes@gmail.com>
2019-12-18 20:34:04 +0000

    Add new pages

---
+++ src/pages/company/team.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Layout from "../../components/layout";
+
+const Team = () => (
+  <Layout>
+    <div>
+      <h1>Our Team</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Praesentium
+        perferendis unde sunt accusantium et molestiae eveniet officia
+        reiciendis iure tempore quod, odio maiores est delectus rem sapiente eum
+        doloribus beatae ipsum! Quam eligendi ducimus, odit eius laudantium nemo
+        consequuntur quia, modi consequatur dolore dicta officiis assumenda
+        sapiente perspiciatis itaque aspernatur.
+      </p>
+    </div>
+  </Layout>
+);
+
+export default Team;
