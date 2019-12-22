921f126c3f0691ef70a81021c7475985eb84e7ba
Gerard Hynes <gkhynes@gmail.com>
2019-12-22 19:55:07 +0000

    Style header with styled components

--- src/components/header.js
+++ src/components/header.js
@@ -1,37 +1,45 @@
 import React from "react";
 import { Link } from "gatsby";
 import PropTypes from "prop-types";
+import styled from "styled-components";
 
 import gatsbyLogo from "../images/gatsby-icon.png";
 
+const HeaderWrapper = styled.header`
+  background: #524763;
+  margin-bottom: 1.45rem;
+  img {
+    margin-bottom: 0;
+  }
+`;
+const HeaderContainer = styled.div`
+  margin: 0 auto;
+  max-width: 960px;
+  padding: 1rem;
+  h1 {
+    margin: 0 0 0 1rem;
+  }
+  a {
+    display: flex;
+    align-items: center;
+  }
+`;
+
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          <img src={gatsbyLogo} alt="Gatsby Logo" style={{ width: "100px" }} />
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
+  <HeaderWrapper>
+    <HeaderContainer>
+      <Link
+        to="/"
+        style={{
+          color: `white`,
+          textDecoration: `none`,
+        }}
+      >
+        <img src={gatsbyLogo} alt="Gatsby Logo" style={{ width: "100px" }} />
+        <h1>{siteTitle}</h1>
+      </Link>
+    </HeaderContainer>
+  </HeaderWrapper>
 );
 
 Header.propTypes = {
